2b7cccd9bb7981349cea565cdcdafefb1458f25a
Taha Salman <tahasalmantsk7@gmail.com>
2025-07-09 04:34:37 +0500

    Deployment Preparation

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from 'path';
 
 import notesRoutes from "./Routes/notesRoutes.js";
 import { connectDB } from "../config/db.js";
@@ -8,17 +9,19 @@ import rateLimiter from "./Middleware/rateLimiter.js";
 
 dotenv.config();
 
-console.log(process.env.MONGO_URI)
-
 const app = express();
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
+const __dirname =path.resolve();
 
 //Middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173"
-  })
-);
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -28,7 +31,15 @@ app.use(rateLimiter);
 //   next();
 // })
 
-app.use("/api/notes", notesRoutes)
+app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
+  });
+}
 
 connectDB().then(() => {
     app.listen(PORT, () => {
--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+//In production, there is no localhost, so we have to handle it dynamically
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" :"/api";
 const api = axios.create({
-  baseURL : "http://localhost:5001/api",
+  baseURL : BASE_URL,
 });
 
 export default api;
