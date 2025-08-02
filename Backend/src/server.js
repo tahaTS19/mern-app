01135d247b10568f56633466e4ebf3ea52891cb3
Taha Salman <tahasalmantsk7@gmail.com>
2025-08-03 03:07:17 +0500

    Deployment Prep (URL Frontend)

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5001
 //Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://notespace-tau.vercel.app/"],
     credentials: true 
   })
 );
